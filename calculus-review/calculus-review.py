1f6d6ffc824c6f22a5b098250848f0699fe3e535
Richard-Zheng <yuqiu36@live.com>
2020-08-04 00:13:10 +0800

    200804-0013

--- calculus-review/calculus-review.py
+++ calculus-review/calculus-review.py
@@ -1,4 +1,5 @@
 from manimlib.imports import *
+import numpy as np
 
 class TangentExplain(Scene):
     CONFIG = {
@@ -18,7 +19,7 @@ class TangentExplain(Scene):
                 "include_numbers": True
             },
         },
-        "func": lambda x: x ** 3,
+        "func": lambda x: x ** 2,
         "func_config": {
             "color": RED,
             "x_min": -1,
@@ -30,13 +31,12 @@ class TangentExplain(Scene):
 
     def construct(self):
         axes = self.get_axes()
-        func = self.axes.get_graph(self.func,**self.func_config)
+        func_graph = self.axes.get_graph(self.func,**self.func_config)
 
         ## Definition
         x = 1
         # ValueTrackers definition
         delta_x_value = ValueTracker(0.5)
-        #delta_fx_value = ValueTracker(self.delta_func(x, delta_x_value.get_value()))
         # DecimalNumber definition
         delta_x_tex = DecimalNumber(delta_x_value.get_value()).add_updater(lambda v: v.set_value(delta_x_value.get_value()))
         delta_fx_tex = DecimalNumber(self.delta_func(x, delta_x_value.get_value())).add_updater(lambda v: v.set_value(self.delta_func(x, delta_x_value.get_value())))
@@ -47,6 +47,11 @@ class TangentExplain(Scene):
         x_dot = Dot(point=self.get_point_from_x_coordinate(x))
         plus_x_dot = Dot(point=self.get_point_from_x_coordinate(x + delta_x_value.get_value()))
         plus_x_dot.add_updater(lambda mob: mob.move_to(self.get_point_from_x_coordinate(x + delta_x_value.get_value())))
+        # Line definition
+        tangent_line = Line(start=x_dot.get_center(), end=plus_x_dot.get_center())
+        tangent_line.set_length(10)
+        # still contain some issues
+        tangent_line.add_updater(lambda mob: mob.put_start_and_end_on(self.get_point_from_x_coordinate(x), self.get_point_from_x_coordinate(x + delta_x_value.get_value())).set_length(10))
 
         ## Grouping
         # all labels and numbers
@@ -56,19 +61,20 @@ class TangentExplain(Scene):
         delta_fx_group = VGroup(delta_fx_tex, delta_fx_label)
 
         ## Set position
-        delta_x_label.next_to(delta_x_tex, LEFT, buff=0.1, aligned_edge=ORIGIN)
-        delta_fx_label.next_to(delta_fx_tex, LEFT, buff=0.1, aligned_edge=ORIGIN)
+        delta_x_label.next_to(delta_x_tex, LEFT, buff=0.2, aligned_edge=ORIGIN)
+        delta_fx_label.next_to(delta_fx_tex, LEFT, buff=0.2, aligned_edge=ORIGIN)
         # Align labels and numbers
-        VGroup(delta_x_group, delta_fx_group).arrange(DOWN, buff=2, aligned_edge=DOWN).to_edge(UR)
+        VGroup(delta_x_group, delta_fx_group).arrange(DOWN, buff=2, aligned_edge=DOWN).to_edge(UR).shift(LEFT)
 
-        # Get NumberLine, Arrow from x
+        ## Create NumberLine
         delta_x_number_line = NumberLine(
             x_min=-0.5,
             x_max=0.5,
             unit_size=3,
-            tick_frequncy=0.1,
+            tick_frequency=0.1,
             include_numbers=True,
         )
+        delta_x_number_line.add_numbers(-0.5, 0, 0.5)
         delta_x_number_line_arrow = self.get_arrow(
             delta_x_number_line,
             delta_x_value.get_value(),
@@ -81,7 +87,6 @@ class TangentExplain(Scene):
             x_min=-1,
             x_max=3,
             unit_size=1,
-            tick_frequncy=2,
             include_numbers=True,
         )
         delta_fx_number_line_arrow = self.get_arrow(
@@ -92,6 +97,7 @@ class TangentExplain(Scene):
         delta_fx_number_line_group = VGroup(delta_fx_number_line, delta_fx_number_line_arrow)
         delta_fx_number_line_group.next_to(delta_fx_group, DOWN, buff=0.5)
 
+        ## Animation
         self.play(
             Write(axes),
             Write(x_dot),
@@ -99,7 +105,8 @@ class TangentExplain(Scene):
             Write(group),
             Write(delta_x_number_line_group),
             Write(delta_fx_number_line_group),
-            ShowCreation(func),
+            Write(tangent_line),
+            ShowCreation(func_graph),
         )
         self.wait()
 
